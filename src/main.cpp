f1d66ddaf24975b9818eed61594bbdedaf94c55b
leizh <zhenglei0418@gmail.com>
2022-09-15 22:02:28 +0200

    acwing

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,9 @@ using namespace std;
 
 int main() {
   string input;
-
-  while (cin >> input) {
-    if (!input.compare("exit")) return (EXIT_SUCCESS);
-  }
+  //while (cin >> input) {
+  //  if (!input.compare("exit")) return (EXIT_SUCCESS);
+  //}
   cout << "Exit code" << endl;
   return (EXIT_FAILURE);
 }
