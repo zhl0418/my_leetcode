bc8497cb7ec41d6546974d260c7c63f35b53805c
leizh <zhenglei0418@gmail.com>
2022-10-25 22:12:29 +0200

    correction 69

--- src/69_sqrt.hpp
+++ src/69_sqrt.hpp
@@ -10,9 +10,11 @@ class Solution_69 {
  public:
   int mySqrt(int x) {
     // Newton Raphson Methode
+    if (x == 1) return 1;
     double a = x / 2;
     while (true) {
       double tmp = a;
+      if (abs(a) < 1e-5) return 0;
       a = a - (a * a - x) / (2 * a);
       if ((int)a == (int)tmp) break;
     }
